14d45b107522146d05eb359d4ccde81b8c06da5c
Gani14 <66427443+Gani14@users.noreply.github.com>
2020-06-06 10:11:26 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server 2"
-echo "This is an example of a script 2"
+echo "This is an example of a script 3"
 echo "This is running in a build step and it must show changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
